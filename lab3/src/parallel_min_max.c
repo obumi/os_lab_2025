9e3cfcef60e86c3cdc7aff943ce19c91e1bb865e
obumi <ivantikhonov7@yandex.ru>
2025-09-20 08:19:21 +0000

    lab3 unfin

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -40,18 +40,24 @@ int main(int argc, char **argv) {
         switch (option_index) {
           case 0:
             seed = atoi(optarg);
-            // your code here
-            // error handling
+            if (seed < 0) {
+              printf("Seed must be non-negative\n");
+              return 1;
+            }
             break;
           case 1:
             array_size = atoi(optarg);
-            // your code here
-            // error handling
+            if (array_size <= 0) {
+              printf("Array size must be positive\n");
+              return 1;
+            }
             break;
           case 2:
             pnum = atoi(optarg);
-            // your code here
-            // error handling
+            if (pnum <= 0) {
+              printf("Pnum must be positive\n");
+              return 1;
+            }
             break;
           case 3:
             with_files = true;
@@ -88,36 +94,106 @@ int main(int argc, char **argv) {
   GenerateArray(array, array_size, seed);
   int active_child_processes = 0;
 
+  //pipes creation
+  
+  int (*pipefd)[2] = NULL;
+  if (!with_files) {
+    pipefd = malloc(sizeof(int[2]) * pnum);
+    if (pipefd == NULL) {
+      perror("malloc");
+      free(array);
+      return 1;
+    }
+    for (int i = 0; i < pnum; i++) {
+      if (pipe(pipefd[i]) == -1) {
+        perror("pipe");
+        // clean up 
+        for (int j = 0; j < i; j++) {
+          close(pipefd[j][0]);
+          close(pipefd[j][1]);
+        }
+        free(pipefd);
+        free(array);
+        return 1;
+      }
+    }
+  }
+
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
 
   for (int i = 0; i < pnum; i++) {
     pid_t child_pid = fork();
     if (child_pid >= 0) {
-      // successful fork
       active_child_processes += 1;
       if (child_pid == 0) {
-        // child process
 
-        // parallel somehow
+        int start = i * (array_size / pnum);
+        int end = (i == pnum - 1) ? array_size : start + (array_size / pnum);
 
-        if (with_files) {
-          // use files here
-        } else {
-          // use pipe here
+        int local_min = INT_MAX;
+        int local_max = INT_MIN;
+        for (int k = start; k < end; k++) {
+          if (array[k] < local_min) local_min = array[k];
+          if (array[k] > local_max) local_max = array[k];
+        }
+
+        if (with_files) { //files
+          char filename[256];
+          snprintf(filename, sizeof(filename), "tmp_minmax_%d.txt", i);
+          FILE *f = fopen(filename, "w");
+          if (f == NULL) {
+            perror("fopen");
+            // Even on error, exit child
+            return 1;
+          }
+          fprintf(f, "%d %d\n", local_min, local_max);
+          fclose(f);
+        } else { //pipe
+          // close read end in child
+          close(pipefd[i][0]); 
+          // write two integers
+          if (write(pipefd[i][1], &local_min, sizeof(local_min)) == -1) {
+            perror("write min");
+            // attempt to close and exit
+            close(pipefd[i][1]);
+            return 1;
+          }
+          if (write(pipefd[i][1], &local_max, sizeof(local_max)) == -1) {
+            perror("write max");
+            close(pipefd[i][1]);
+            return 1;
+          }
+          // close write end after writing
+          close(pipefd[i][1]);
         }
         return 0;
+      } else {
+        // parent process
+        if (!with_files) {
+          // parent doesn't need write end
+          close(pipefd[i][1]);
+        }
       }
 
     } else {
       printf("Fork failed!\n");
+      // cleanup pipes if allocated
+      if (pipefd) {
+        for (int j = 0; j < pnum; j++) {
+          close(pipefd[j][0]);
+          close(pipefd[j][1]);
+        }
+        free(pipefd);
+      }
+      free(array);
       return 1;
     }
   }
 
   while (active_child_processes > 0) {
     // your code here
-
+    wait(NULL);
     active_child_processes -= 1;
   }
 
@@ -131,8 +207,36 @@ int main(int argc, char **argv) {
 
     if (with_files) {
       // read from files
+      char filename[256];
+      snprintf(filename, sizeof(filename), "tmp_minmax_%d.txt", i);
+      FILE *f = fopen(filename, "r");
+      if (f == NULL) {
+        perror("fopen read");
+        // treat as error: continue
+      } else {
+        if (fscanf(f, "%d %d", &min, &max) != 2) {
+          // reading failed
+          perror("fscanf");
+        }
+        fclose(f);
+        remove(filename);
+      }
     } else {
       // read from pipes
+      // read two integers from read end
+      ssize_t r = read(pipefd[i][0], &min, sizeof(min));
+      if (r == -1) {
+        perror("read min");
+      } else if (r == 0) {
+        // no data
+      }
+      r = read(pipefd[i][0], &max, sizeof(max));
+      if (r == -1) {
+        perror("read max");
+      } else if (r == 0) {
+        // no data
+      }
+      close(pipefd[i][0]);
     }
 
     if (min < min_max.min) min_max.min = min;
@@ -146,6 +250,7 @@ int main(int argc, char **argv) {
   elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
 
   free(array);
+  if (pipefd) free(pipefd);
 
   printf("Min: %d\n", min_max.min);
   printf("Max: %d\n", min_max.max);
---
+++ lab3/t.md
@@ -0,0 +1,4 @@
+@obumi ➜ /workspaces/os_lab_2025/lab3/src (master) $ gcc -o sequential_min_max sequential_min_max.c find_min_max.c utils.c
+@obumi ➜ /workspaces/os_lab_2025/lab3/src (master) $ ./sequential_min_max 42 10
+min: 53523743
+max: 1854614940

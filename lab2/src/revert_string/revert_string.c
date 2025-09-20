ef60cd65dea3483a7942c30f028f58dd7a00a0a9
obumi <ivantikhonov7@yandex.ru>
2025-09-20 06:45:51 +0000

    actual fix

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
+#include <string.h>
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+    if (!str) return;
+    char *l = str;
+    char *r = str + strlen(str);
+    if (r == str) return;
+    r--;
+    while (l < r) {
+        char tmp = *l;
+        *l = *r;
+        *r = tmp;
+        l++; r;
+    }
 }
-
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,7 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+ char tmp = *right;
+    *right = *left;
+    *left = tmp;
 }
